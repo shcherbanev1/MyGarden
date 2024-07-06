922875793961440a790d9fde77079a1b18549fb9
shcherbanev1 <shcherbanev1@yandex.ru>
2024-07-06 14:20:48 +0300

    fix: update plantDao

--- app/src/main/java/ru/itis/mygarden/PlantDao.kt
+++ app/src/main/java/ru/itis/mygarden/PlantDao.kt
@@ -1,20 +1,31 @@
 package ru.itis.mygarden
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface PlantDao {
+
     @Query("SELECT * FROM plants ORDER BY id ASC")
-    fun allPlants(): List<Plant>
+    suspend fun getAllPlants(): List<Plant>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertPlant(plant: Plant)
 
+    @Update
     suspend fun updatePlant(plant: Plant)
 
+    @Delete
     suspend fun deletePlant(plant: Plant)
+
+    @Query("SELECT * FROM plants WHERE id = :id")
+    suspend fun getPlantById(id: Int): Plant?
+
+    @Query("SELECT * FROM plants WHERE name = :name")
+    suspend fun getPlantByName(name: String): Plant?
 
 }
