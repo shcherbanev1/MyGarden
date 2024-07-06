d02b4e3b7f9095aaa84d929fde9a889e328f25cb
Almaz <almaz.kzn2005@yandex.ru>
2024-07-06 18:57:06 +0300

    feat: design fragment_create

---
+++ app/src/main/java/ru/itis/mygarden/fragments/create/CreateFragment.kt
@@ -0,0 +1,23 @@
+package ru.itis.mygarden.fragments.create
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import ru.itis.mygarden.R
+import ru.itis.mygarden.databinding.FragmentCreateBinding
+import ru.itis.mygarden.databinding.FragmentInfoBinding
+
+class CreateFragment : Fragment(R.layout.fragment_create) {
+
+    private var binding: FragmentCreateBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCreateBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
