4576b658e9547193af691038777aa2a41a0cddc3
shcherbanev1 <shcherbanev1@yandex.ru>
2024-07-06 14:00:54 +0300

    fix: adjust plant entity implementation

--- app/src/main/java/ru/itis/mygarden/Plant.kt
+++ app/src/main/java/ru/itis/mygarden/Plant.kt
@@ -5,17 +5,25 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "plants")
-class Plant(
+data class Plant(
     @PrimaryKey(autoGenerate = true)
-    var id: Int = 0,
-    @ColumnInfo(name = "name")
-    var name: String,
-    @ColumnInfo(name = "isWatered")
-    var isWatered: Boolean,
-    @ColumnInfo(name = "imgSource")
-    var imgSource: String,
-    @ColumnInfo(name = "sunlight")
-    var sunlight: String
-) {
+    val id: Int = 0,
 
-}
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "description")
+    val description: String?,
+
+    @ColumnInfo(name = "light_preference")
+    val sunlight: String?,
+
+    @ColumnInfo(name = "next_watering_time")
+    var nextWateringTime: Long?,
+
+    @ColumnInfo(name = "watering_frequency")
+    val wateringFrequency: Int,
+
+    @ColumnInfo(name = "img_source")
+    var imgSource: String?,
+)
