723e8c4bcf7addc159c9e9f69fd2563d742cb76c
shcherbanev1 <shcherbanev1@yandex.ru>
2024-07-07 17:20:58 +0300

    fix: redo dao injection into ViewModel

--- app/src/main/java/ru/itis/mygarden/data/PlantViewModel.kt
+++ app/src/main/java/ru/itis/mygarden/data/PlantViewModel.kt
@@ -1,12 +1,18 @@
 package ru.itis.mygarden.data
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.lang.ref.WeakReference
 
-class PlantViewModel(private val plantDao: PlantDao) : ViewModel() {
+class PlantViewModel(context: Context) : ViewModel() {
+    private val contextRef: WeakReference<Context> = WeakReference(context)
+    private val plantDao: PlantDao = contextRef.get()?.let {
+        PlantDatabase.getDataBase(it).plantDao()
+    } ?: throw IllegalStateException("Context is null")
 
     suspend fun getAllPlants(): List<Plant> {
         return withContext(Dispatchers.IO) {
---
+++ app/src/main/java/ru/itis/mygarden/data/PlantViewModelFactory.kt
@@ -0,0 +1,16 @@
+package ru.itis.mygarden.data
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class PlantViewModelFactory(private val context: Context) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(PlantViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return PlantViewModel(context) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
