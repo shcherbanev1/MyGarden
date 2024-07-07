1114c6c56b40c4552f1e88b6f9b50a2d0ba5d462
Almaz <almaz.kzn2005@yandex.ru>
2024-07-07 23:25:37 +0300

    feat: add mock storage

---
+++ app/src/main/java/ru/itis/mygarden/data/MockStorage.kt
@@ -0,0 +1,232 @@
+package ru.itis.mygarden.data
+////sk-4sHM668aad99c3f496168
+object MockStorage {
+
+    val plants: List<Plant> = listOf(
+        Plant(
+            id = 0,
+            name = "Test Plant",
+            description = "Description of the test plant",
+            sunlight = "full_shade",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 3,
+            imgSource = "https://gagaru.club/uploads/posts/2023-06/1685997476_gagaru-club-p-rastenie-v-gorshke-vkontakte-48.jpg"
+        ),
+        Plant(
+            id = 1,
+            name = "European Silver Fir",
+            description = "Description of the European Silver Fir",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 3,
+            imgSource = "https://perenual.com/storage/species_image/1_abies_alba/og/1536px-Abies_alba_SkalitC3A9.jpg"
+        ),
+        Plant(
+            id = 1607,
+            name = "rose peashrub",
+            description = "Description of the rose peashrub",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 1,
+            imgSource = "https://perenual.com/storage/species_image/1607_caragana_rosea/og/9216111658_14dbc2a85c_b.jpg"
+        ),
+        Plant(
+            id = 2,
+            name = "Pyramidalis Silver Fir",
+            description = "Description of the Pyramidalis Silver Fir",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 3,
+            imgSource = "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/og/49255769768_df55596553_b.jpg"
+        ),
+        Plant(
+            id = 61,
+            name = "Hogyoku Japanese Maple",
+            description = "Description of the Hogyoku Japanese Maple",
+            sunlight = "part_shade",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 3,
+            imgSource = "https://perenual.com/storage/species_image/61_acer_palmatum_hogyoku/og/2560px-Kyoto_Japan0431.jpg"
+        ),
+        Plant(
+            id = 271,
+            name = "Naylor's Blue Leyland Cypress",
+            description = "Description of the Naylor's Blue Leyland Cypress",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 4,
+            imgSource = "https://perenual.com/storage/species_image/271_cupressocyparis_x_leylandii_naylors_blue/og/Cupressocyparis_leylandii_27Naylor27s_Blue27_OB_WrocC582aw.jpg"
+        ),
+        Plant(
+            id = 494,
+            name = "Siberian yarrow",
+            description = "Description of the Siberian yarrow",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 3,
+            imgSource = "https://perenual.com/storage/species_image/494_achillea_sibirica/og/7762255084_e1def57a0c_b.jpg"
+        ),
+        Plant(
+            id = 841,
+            name = "Texas firecracker",
+            description = "Description of the Texas firecracker",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 3,
+            imgSource = "https://perenual.com/storage/species_image/841_anisacanthus_quadrifidus_var_wrightii/og/2229260565_617fc8a76d_b.jpg"
+        ),
+        Plant(
+            id = 865,
+            name = "Indian hemp",
+            description = "Description of the Indian hemp",
+            sunlight = "part_shade",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 1,
+            imgSource = "https://perenual.com/storage/species_image/865_apocynum_cannabinum/og/52279327528_0d7dd70f2c_b.jpg"
+        ),
+        Plant(
+            id = 1023,
+            name = "Sprenger's asparagus fern",
+            description = "Description of the Sprenger's asparagus fern",
+            sunlight = "part_shade",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 3,
+            imgSource = "https://perenual.com/storage/species_image/1023_asparagus_densiflorus/og/49289066276_4bbe4e5b94_b.jpg"
+        ),
+        Plant(
+            id = 1987,
+            name = "Japanese clethra",
+            description = "Description of the Japanese clethra",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 10,
+            imgSource = "https://perenual.com/storage/species_image/1988_clethra_pringlei/og/52569271743_c02336422c_b.jpg"
+        ),
+        Plant(
+            id = 2101,
+            name = "tatarian dogwood",
+            description = "Description of the tatarian dogwood",
+            sunlight = "part_shade",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 3,
+            imgSource = "https://perenual.com/storage/species_image/2101_cornus_alba_bailhalo_ivory_halo/og/14927994662_3aef16c979_b.jpg"
+        ),
+        Plant(
+            id = 2491,
+            name = "common foxglove",
+            description = "Description of the common foxglove",
+            sunlight = "part_shade",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 3,
+            imgSource = "https://perenual.com/storage/species_image/2491_digitalis_purpurea_foxy_group/og/50020593372_3f8c3870d8_b.jpg"
+        ),
+        Plant(
+            id = 2641,
+            name = "purple coneflower",
+            description = "Description of the purple coneflower",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 1,
+            imgSource = "https://perenual.com/storage/species_image/2641_echinacea_purpurea_merlot/og/flowers-echinacea-bee-summer-nature-flora.jpg"
+        ),
+        Plant(
+            id = 2701,
+            name = "small globe thistle",
+            description = "Description of the small globe thistle",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 1,
+            imgSource = "https://perenual.com/storage/species_image/2701_echinops_ritro_veitchs_blue/og/14715614414_c35259e033_b.jpg"
+        ),
+        Plant(
+            id = 2791,
+            name = "common fleabane",
+            description = "Description of the common fleabane",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 1,
+            imgSource = "https://perenual.com/storage/species_image/2791_erigeron_philadelphicus/og/49920924763_a1a07d1c05_b.jpg"
+        ),
+        Plant(
+            id = 2805,
+            name = "sea holly",
+            description = "Description of the sea holly",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 1,
+            imgSource = "https://perenual.com/storage/species_image/2805_eryngium_planum_jade_frost/og/6950161190_5b7a9a2ccd_b.jpg"
+        ),
+        Plant(
+            id = 1903,
+            name = "clematis",
+            description = "Description of the clematis",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 3,
+            imgSource = "https://perenual.com/storage/species_image/1903_clematis_belle_of_taranaki/og/Clematis_diamantina_evipo039.jpg"
+        ),
+        Plant(
+            id = 2994,
+            name = "border forsythia",
+            description = "Description of the border forsythia",
+            sunlight = "part_shade",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 3,
+            imgSource = "https://perenual.com/storage/species_image/2994_forsythia_intermedia_sunrise/og/4592591556_84b8f7f51b_b.jpg"
+        ),
+        Plant(
+            id = 2941,
+            name = "tall fescue",
+            description = "Description of the tall fescue",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 3,
+            imgSource = "https://perenual.com/storage/species_image/2941_festuca_arundinacea_mix/og/2560px-Festuca_arundinacea.jpg"
+        ),
+        Plant(
+            id = 2953,
+            name = "fig buttercup",
+            description = "Description of the fig buttercup",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 10,
+            imgSource = "https://perenual.com/storage/species_image/2953_ficaria_verna/og/52184617072_d6a82b4f77_b.jpg"
+        ),
+        Plant(
+            id = 2958,
+            name = "fig",
+            description = "Description of the fig",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 3,
+            imgSource = "https://perenual.com/storage/species_image/2958_ficus_carica_chicago_hardy/og/croatia-mljet-fig-leavs.jpg"
+        ),
+        Plant(
+            id = 2970,
+            name = "queen of the prairie",
+            description = "Description of the queen of the prairie",
+            sunlight = "part_shade",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 10,
+            imgSource = "https://perenual.com/storage/species_image/2970_filipendula_rubra/og/51294407118_8de128012a_b.jpg"
+        ),
+        Plant(
+            id = 1591,
+            name = "canna",
+            description = "Description of the canna",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 3,
+            imgSource = "https://perenual.com/storage/species_image/1591_canna_striata/og/1536px-Canna_27Striata27_Paciorecznik_2019-09-15_03.jpg"
+        ),
+        Plant(
+            id = 1891,
+            name = "meyer lemon",
+            description = "Description of the meyer lemon",
+            sunlight = "full_sun",
+            nextWateringTime = 1720515600L,
+            wateringFrequency = 1,
+            imgSource = "https://perenual.com/storage/species_image/1891_citrus_meyeri/og/52503082796_4e584f5e18_b.jpg"
+        )
+    )
+}
