2078df3445cef5092cd4c167132d3e9f3fec73e6
shcherbanev1 <shcherbanev1@yandex.ru>
2024-07-07 13:56:00 +0300

    fix: delete viewModel initialization from MainActivity

--- app/src/main/java/ru/itis/mygarden/MainActivity.kt
+++ app/src/main/java/ru/itis/mygarden/MainActivity.kt
@@ -2,19 +2,14 @@ package ru.itis.mygarden
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import ru.itis.mygarden.data.PlantViewModel
 import ru.itis.mygarden.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-  
     private var binding: ActivityMainBinding? = null
     private var controller: NavController? = null
-    private var plantViewModel: PlantViewModel? = null
-  
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate((layoutInflater))
@@ -24,9 +19,5 @@ class MainActivity : AppCompatActivity() {
         controller?.let { navController ->
             binding?.bottomNavigation?.setupWithNavController(navController)
         }
-        
-        val application = application as PlantApplication
-        plantViewModel = application.viewModel
-
     }
 }
