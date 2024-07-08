cb788245134b00077fe7fb235b4b3061a0111137
IMART0 <imartirosyan@vk.com>
2024-07-08 07:09:14 +0300

    feat: crate PlantHolder.kt and PlantAdapter.kt

---
+++ app/src/main/java/ru/itis/mygarden/fragments/plant/PlantAdapter.kt
@@ -0,0 +1,32 @@
+package ru.itis.mygarden.fragments.plant
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import ru.itis.mygarden.data.Plant
+import ru.itis.mygarden.databinding.UserPlantsItemBinding
+
+class PlantAdapter(
+    private val plants: List<Plant>,
+    private val glide: RequestManager,
+    private val onClick: Unit
+    ): RecyclerView.Adapter<PlantHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlantHolder {
+            return PlantHolder(
+                binding = UserPlantsItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                ),
+                glide = glide,
+                onClick = onClick
+            )
+        }
+
+        override fun getItemCount(): Int = plants.size
+
+        override fun onBindViewHolder(holder: PlantHolder, position: Int) {
+            holder.onBind(plants[position])
+        }
+}
--- app/src/main/java/ru/itis/mygarden/fragments/plant/PlantFragment.kt
+++ app/src/main/java/ru/itis/mygarden/fragments/plant/PlantFragment.kt
@@ -3,20 +3,49 @@ package ru.itis.mygarden.fragments.plant
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import ru.itis.mygarden.R
 import ru.itis.mygarden.databinding.FragmentPlantBinding
+import ru.itis.mygarden.presentation.PlantViewModel
+import ru.itis.mygarden.presentation.PlantViewModelFactory
 
 class PlantFragment : Fragment(R.layout.fragment_plant) {
 
     private var binding: FragmentPlantBinding? = null
+    private var adapter: PlantAdapter? = null
+    private lateinit var viewModel: PlantViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val context = requireContext()
+        viewModel = ViewModelProvider(this, PlantViewModelFactory(context)).get(PlantViewModel::class.java)
+        initAdapter()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val context = requireContext()
         binding = FragmentPlantBinding.bind(view)
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+    private fun initAdapter() {
+        adapter = PlantAdapter(
+            plants = viewModel.getAllPlants(),
+            glide = Glide.with(this),
+            onClick = findNavController().navigate(
+                resId = R.id.action_plantFragment_to_addFragment,
+            )
+        )
+        binding?.run {
+            userPlantsRv.adapter = adapter
+        }
     }
 }
